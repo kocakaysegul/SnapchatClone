16f42fb0279c748ef398845d15c17dcac4a6fe17
kocakaysegul <aysegulkocak112@gmail.com>
2023-03-26 06:53:34 +0300

    User Interface designed

---
+++ SnapchatClone/ViewControllers/SettingsVC.swift
@@ -0,0 +1,33 @@
+//
+//  SettingsVC.swift
+//  SnapchatClone
+//
+//  Created by Ayşegül Koçak on 26.03.2023.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    @IBAction func logoutClicked(_ sender: Any) {
+    }
+    
+}
